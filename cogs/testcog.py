11c81af7f4f575bd0b57fadbb20ca0037e37c9d5
LitenKanin <30553963+LitenKanin@users.noreply.github.com>
2018-02-13 18:43:43 +0100

    Update testcog.py

--- cogs/testcog.py
+++ cogs/testcog.py
@@ -1,13 +1,50 @@
-# Discord and Caverna
 import discord
-from .utils import checks
-from __main__ import send_cmd_help
-from .utils.dataIO import dataIO
 from discord.ext import commands
+from .utils.chat_formatting import escape_mass_mentions, italics, pagify
+from random import randint
+from random import choice
+from enum import Enum
+from urllib.parse import quote_plus
+import datetime
+import time
+import aiohttp
+import asyncio
+
+settings = {"POLL_DURATION" : 60}
 
 
-    @commands.command(pass_context=True, no_pm=True)
-    async def testcog(self, ctx):
-        """Carrot settings group command"""
+class RPS(Enum):
+    rock     = "\N{MOYAI}"
+    paper    = "\N{PAGE FACING UP}"
+    scissors = "\N{BLACK SCISSORS}"
 
-        await self.bot.say("there's nothing wrong with the other cogs, perhaps check the code of this one")
+
+class RPSParser:
+    def __init__(self, argument):
+        argument = argument.lower()
+        if argument == "rock":
+            self.choice = RPS.rock
+        elif argument == "paper":
+            self.choice = RPS.paper
+        elif argument == "scissors":
+            self.choice = RPS.scissors
+        else:
+            raise
+
+
+class General:
+    """General commands."""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.stopwatches = {}
+        self.ball = ["As I see it, yes", "It is certain", "It is decidedly so", "Most likely", "Outlook good",
+                     "Signs point to yes", "Without a doubt", "Yes", "Yes – definitely", "You may rely on it", "Reply hazy, try again",
+                     "Ask again later", "Better not tell you now", "Cannot pCavernaict now", "Concentrate and ask again",
+                     "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+        self.poll_sessions = []
+
+    @commands.command()
+    async def devlogtest(self, *choices):
+
+            await self.bot.say("The cog works, but check code for troubleshooting")
