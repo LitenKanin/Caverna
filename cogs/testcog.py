fc3aab85af9b82f18d4276bfb42138c157834c6e
LitenKanin <30553963+LitenKanin@users.noreply.github.com>
2018-02-13 18:11:52 +0100

    Update testcog.py

--- cogs/testcog.py
+++ cogs/testcog.py
@@ -1,14 +1,5 @@
 import discord
 from discord.ext import commands
-from .utils.chat_formatting import escape_mass_mentions, italics, pagify
-from random import randint
-from random import choice
-from enum import Enum
-from urllib.parse import quote_plus
-import datetime
-import time
-import aiohttp
-import asyncio
 
 @commands.command(pass_context=True)
 async def yt(ctx, url):
