05ef2203ab8efcf19be8381b1305a69bbc098b7e
LitenKanin <30553963+LitenKanin@users.noreply.github.com>
2018-04-10 15:35:45 +0200

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -502,8 +502,9 @@ class Mod:
         return
     if not member:
         return await client.say(ctx.message.author.mention + "Specify a user to DM!")
-    if member = "@everyone":
-        member = 
+    if member == "@everyone":
+         for server_member in ctx.message.server.members:
+              await client.send_message(server_member, message)
     else:
         await client.send_message(member, message)
         
